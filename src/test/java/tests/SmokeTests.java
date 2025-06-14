fc8749b8760c10020c012f557f829d50ada51cb9
trubnikov <trubnikov@jivosite.com>
2025-06-15 01:27:45 +0300

    Скорректировано название папки tests + перенес AllureListener в @BeforeEach + убрал лишнюю строку conf.remote + убрал лишние зависимости + tasks.withType(Test) поправил

--- src/test/java/tests/SmokeTests.java
+++ src/test/java/tests/SmokeTests.java
@@ -1,13 +1,10 @@
-package tests.ui;
+package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -29,14 +26,16 @@ public class SmokeTests {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
 
+    @BeforeEach
+    void beforeEach() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
@@ -128,7 +127,7 @@ public class SmokeTests {
     }
 
         @Test
-        @DisplayName("rm -rf .git\n")
+        @DisplayName("Проверяем работу счётчика в корзине")
         void verifyAddingAnalysesToCart() {
             step("Открываем главную страницу заказа анализов", () -> {
                 open("https://lk.sberhealth.ru/laboratory-analyses");
